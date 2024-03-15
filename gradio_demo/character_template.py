8c6062cc70eae6cf2ad1143c9ba5a6a34e295462
kongzhecn <964453125@qq.com>
2024-03-15 23:00:17 +0800

    SUBMIT

--- gradio_demo/character_template.py
+++ gradio_demo/character_template.py
@@ -3,7 +3,7 @@ character_list_man = [
         "name": "Harry Potter (identifier: Harry Potter)",
         "prompt": "Close-up photo of the Harry Potter, 35mm photograph, film, professional, 4k, highly detailed.",
         "negative_prompt": "noisy, blurry, soft, deformed, ugly",
-        "path": "../checkpoint/lora/Harry_Potter.safetensors",
+        "path": "./checkpoint/lora/Harry_Potter.safetensors",
     },
     {
         "name": "Chris Evans (identifier: Chris Evans)",
@@ -18,7 +18,7 @@ character_list_woman = [
         "name": "Hermione Granger (identifier: Hermione Granger)",
         "prompt": "Close-up photo of the Hermione Granger, 35mm photograph, film, professional, 4k, highly detailed.",
         "negative_prompt": "noisy, blurry, soft, deformed, ugly",
-        "path": "../checkpoint/lora/Hermione_Granger.safetensors",
+        "path": "./checkpoint/lora/Hermione_Granger.safetensors",
     },
     {
         "name": "Taylor Swift (identifier: TaylorSwift)",
